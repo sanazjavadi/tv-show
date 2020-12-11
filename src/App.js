a60eb9988addf71a560ac400940c259e277aaaca
sanaz_javadi <sanazjavadi72@yahoo.com>
2020-12-11 18:33:08 +0330

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+//components
+import Navbar from './components/Navbar';
+
+//Pages
+import HomePage from "./pages/Homepage";
+import AboutPage from "./pages/Aboutpage"
+import SinglePage from "./pages/Singlepage"
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+<Navbar/>
+<Switch>
+  <Route exact path="/" component={HomePage} / >
+    <Route path="/about" component={AboutPage} />
+    <Route path="/singleshow/:id" component={SinglePage} />
+</Switch>
+
+
+    </Router>
   );
 }
 
